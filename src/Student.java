a930f6f58671f8f21a07106467fb54842bb44e63
joshua <joshua.benyobaker@menloschool.org>
2023-01-12 14:56:53 -0800

    Started Person and Student

--- src/Student.java
+++ src/Student.java
@@ -7,6 +7,17 @@
  * @version: 2022-2023
  */
 
-public class Student {
+public class Student extends Person
+{
     // TODO: Modify and complete this class
+
+    int grade;
+
+    public Student(String firstName, String lastName, String phoneNumber, int grade)
+    {
+        super(firstName, lastName, phoneNumber);
+        this.grade = grade;
+    }
+
+
 }
